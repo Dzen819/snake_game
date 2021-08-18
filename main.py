89a88954d2e2d9d9a1cc2169964459f08cf3d956
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-08-18 21:52:14 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
+snake = Snake()
+food = Food()
+screen.listen()
 
-snake_body = []
-for i in range(3):
-    snake = Turtle()
-    snake.color("white")
-    snake.shape("square")
-    snake.goto(snake.xcor() + 20*i, 0)
-    snake_body.append(snake)
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-
-
-
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 
 
---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+
+class Snake:
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+        for i in range(3):
+            snake = Turtle()
+            snake.color("white")
+            snake.shape("square")
+            snake.up()
+            snake.goto(snake.xcor() - 20 * i, 0)
+            self.snake_body.append(snake)
+
+    def move(self):
+
+        for snk in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[snk - 1].xcor()
+            new_y = self.snake_body[snk - 1].ycor()
+            self.snake_body[snk].goto(new_x, new_y)
+        self.head.forward(20)
+        if self.head.xcor() > 280:
+            self.head.goto(-280, self.head.ycor())
+        elif self.head.xcor() < -280:
+            self.head.goto(280, self.head.ycor())
+        elif self.head.ycor() < -280:
+            self.head.goto(self.head.xcor(), 280)
+        elif self.head.ycor() > 280:
+            self.head.goto(self.head.xcor(), -280)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
